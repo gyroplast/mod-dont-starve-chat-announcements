285286e9509a4464f45099eaf6b34712c86f6915
Dennis Herbrich <kontakt+github@veloxis.de>
2021-09-11 23:50:36 +0200

    update package.sh for Steam ModUploader
    The package script now links the path required by the ModUploader, i. e.
    ./out/steam/Chat_Announcements, to the prepared release directory.
    Also, the release directory and zipfile do not have spaces in their
    names anymore, which have been replaced by underscores.
    
    Signed-off-by: Dennis Herbrich <kontakt+github@veloxis.de>

--- package.sh
+++ package.sh
@@ -3,12 +3,17 @@ set -e
 set -x
 _modname=$(sed -n '/^\s*name\s*=/{s/.*"\(.\+\).*"/\1/p;q}' modinfo.lua)
 _modversion=$(sed -n '/^\s*version\s*=/{s/.*"\(.\+\).*"/\1/p;q}' modinfo.lua)
-_OUT=$(readlink -m -n "./out/${_modname}-${_modversion}")
+_OUT=$(readlink -m -n "./out/${_modname// /_}-${_modversion}")
+_OUTSTEAM=$(readlink -m -n "./out/steam/${_modname// /_}")
 rm -fr "${_OUT}" "${_OUT}.zip"
 
 for f in README.md LICENSE modicon.* modinfo.lua modmain.lua lib/*; do
   install -Dm755 "$f" "${_OUT}/${f}"
 done
+
+# the ModUploader REALLY wants the mod to be in this subdirectory
+install -dm755 "$(dirname "${_OUTSTEAM}")"
+ln -sf "${_OUT}" "${_OUTSTEAM}"
 
 pushd "${_OUT}"/..
 zip -r9 "${_OUT}.zip" "$(basename "${_OUT}")"
